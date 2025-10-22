ed554c75a6222e644576c9ce3c409e8d01d69e87
BonkaiCPP <brhernandez@gbox.adnu.edu.ph>
2025-10-22 23:48:14 +0800

    NOTIFICATIONPAGE

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -31,7 +31,9 @@ const App = () => {
         )} />
         <Route path = "/signup" element = {!isAuthenticated ? <SignUpPage /> : <Navigate to = "/" /> } />
         <Route path = "/login" element = {!isAuthenticated ? <LoginPage /> : <Navigate to = "/" /> } />
-        <Route path = "/notifications" element = {isAuthenticated ? <NotificationsPage /> : <Navigate to="/login" /> } />
+        <Route path = "/notifications" element = {isAuthenticated && isOnboarded ? (<Layout showSidebar = {true}>
+          <NotificationsPage />
+        </Layout>) : (<Navigate to={!isAuthenticated ? "/login" : "/onboarding"} />)} />
         <Route path = "/call" element = {isAuthenticated ? <CallPage /> : <Navigate to="/login" />} />
         <Route path = "/chat" element = {isAuthenticated ? <ChatPage /> : <Navigate to="/login" />} />
         <Route path = "/onboarding" element = {isAuthenticated ? (
--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -15,3 +15,13 @@ export const completeOnboarding = async (userData) => {
   const response = await axiosInstance.post("/auth/onboarding", userData);
   return response.data;
 };
+
+export const getFriendRequests = async () => {
+  const response = await axiosInstance.get("/user/friend-requests");
+  return response.data;
+};
+
+export const acceptFriendRequest = async (requestId) => {
+  const response = await axiosInstance.post(`/user/accept-friend-request/${requestId}/accept`);
+  return response.data;
+};
