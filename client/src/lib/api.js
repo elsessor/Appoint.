e7a8d6e9eb7e6e6612050ea6e706aa5f9b002921
Carl <cevalenciano@gbox.adnu.edu.ph>
2025-10-23 11:33:27 +0800

    HOMEPAGE

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,30 +1,6 @@
 import { axiosInstance } from "./axios";
 import axios from "axios";
 
-export const signup = async (signupData) => {
-  const response = await axiosInstance.post("/auth/signup", signupData);
-  return response.data;
-};
-
-export const getAuthUser = async () => {
-  const res = await axiosInstance.get("/auth/me");
-  return res.data;
-};
-
-export const completeOnboarding = async (userData) => {
-  const response = await axiosInstance.post("/auth/onboarding", userData);
-  return response.data;
-};
-
-export const getFriendRequests = async () => {
-  const response = await axiosInstance.get("/user/friend-requests");
-  return response.data;
-};
-
-export const acceptFriendRequest = async (requestId) => {
-  const response = await axiosInstance.post(`/user/accept-friend-request/${requestId}/accept`);
-  return response.data;
-};
 
 export async function getUserFriends () {
   const response = await axiosInstance.get("/users/friends");
