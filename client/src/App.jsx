8a1994beb53e060f7161883ff0e69f492c07bdaf
elsessor <lagad@gbox.adnu.edu.ph>
2025-10-24 22:41:41 +0800

    Revert "Delete client directory"
    This reverts commit 0dd9b5ad040c840588b4042341e7f9324c7295cd.

---
+++ client/src/App.jsx
@@ -0,0 +1,51 @@
+import { Routes, Route, Navigate, BrowserRouter } from "react-router";
+
+import HomePage from "./pages/Homepage.jsx";
+import SignUpPage from "./pages/SignUpPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import NotificationsPage from "./pages/NotificationsPage.jsx";
+import CallPage from "./pages/CallPage.jsx";
+import ChatPage from "./pages/ChatPage.jsx";
+import OnboardingPage from "./pages/OnboardingPage.jsx";
+
+import { Toaster } from "react-hot-toast";
+import PageLoader from "./components/PageLoader.jsx";
+import { getAuthUser } from "./lib/api.js";
+import useAuthUser from "./hooks/useAuthUser.js";
+
+const App = () => {
+
+  const {isLoading, authUser} = useAuthUser();
+
+  if (isLoading) return <PageLoader />;
+
+  const isAuthenticated = Boolean(authUser);
+  const isOnboarded = authUser?.isOnboarded;
+
+  return <div className="h-screen" data-theme="night">
+      <Routes>
+        <Route path = "/" element = {isAuthenticated && isOnboarded ? (
+          <HomePage />
+        ) : (
+          <Navigate to={!isAuthenticated ? "/login" : "/onboarding"} />
+        )} />
+        <Route path = "/signup" element = {!isAuthenticated ? <SignUpPage /> : <Navigate to = "/" /> } />
+        <Route path = "/login" element = {!isAuthenticated ? <LoginPage /> : <Navigate to = "/" /> } />
+        <Route path = "/notifications" element = {isAuthenticated ? <NotificationsPage /> : <Navigate to="/login" /> } />
+        <Route path = "/call" element = {isAuthenticated ? <CallPage /> : <Navigate to="/login" />} />
+        <Route path = "/chat" element = {isAuthenticated ? <ChatPage /> : <Navigate to="/login" />} />
+        <Route path = "/onboarding" element = {isAuthenticated ? (
+          !isOnboarded ? (
+            <OnboardingPage />
+          ) : (
+            <Navigate to="/" />
+          )
+        ) : (
+          <Navigate to="/login" />
+        )} />
+      </Routes>
+
+    <Toaster />
+  </div>;
+};
+export default App;
