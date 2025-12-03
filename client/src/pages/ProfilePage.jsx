44bae6035d5c268d4c1d408993ce2f13242014d1
BonkaiCPP <brhernandez@gbox.adnu.edu.ph>
2025-12-03 20:40:55 +0800

    Updated

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -11,13 +11,12 @@ const ProfilePage = () => {
   const [isUploadingPhoto, setIsUploadingPhoto] = useState(false);
   const [profile, setProfile] = useState({
     name: '',
-    location: 'Camarines Sur, Philippines',
-    phone: '09479067912',
-    twitter: '@loremipsum',
-    github: '@loremipsum',
-    linkedin: 'loremipsum',
-    about:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.\n\nDuis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    location: '',
+    phone: '',
+    twitter: '',
+    github: '',
+    linkedin: '',
+    about: '',
   });
   const [draft, setDraft] = useState(profile);
   const draftRef = useRef(draft);
